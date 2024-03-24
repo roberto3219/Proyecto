5d850a7050213973f64f4d61d094dc11cc96944c
roberto3219 <llamparoberto5@gmail.com>
2024-03-24 15:37:50 -0300

    sigamos

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+//Librerias
 const express = require("express")
 const app = express();
 
+
+//Servidor
+
+let port = 3009;
+
+app.listen(port, ()=> console.log(`[server] Corriendo en el puerto ${port}`))
+
+//Rutas
+
+app.use("/",mainRouter);
+app.use("/product",productRouter);
+app.use("/cart", cartRoutes);
+app.use("/users", userRoutes);
+
+//Ruta Apis
+
+app.use(productoRouter)
+app.use(usuarioRouter)
+
+//Error 404
+
+app.use((req,res,next) => {
+    res.status(404).render("error404")
+})
